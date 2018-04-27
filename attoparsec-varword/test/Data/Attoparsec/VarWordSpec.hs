152b6c7acaf1e3eda4b73457b2bd78f2ac3f45a1
Paul Weaver <paul@concertdaw.co.uk>
2018-04-27 17:56:41 +0100

    Add some parser testing using new encoders

--- attoparsec-varword/test/Data/Attoparsec/VarWordSpec.hs
+++ attoparsec-varword/test/Data/Attoparsec/VarWordSpec.hs
@@ -4,21 +4,34 @@ import Test.Hspec
 import Test.QuickCheck
 import Data.Attoparsec.ByteString (Parser, parseOnly)
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
 import Data.Word
+
+import Data.ByteString.Builder (Builder)
+import qualified Data.ByteString.Builder as Builder
+import qualified Data.ByteString.Builder.VarWord as Builder
 
 import Data.Attoparsec.VarWord (varWordBe, denseVarWordBe, varWordLe)
 
 spec :: Spec
 spec = do
-  varNumSpec "Big-endian chunked number parser" varWordBe 18464
-  varNumSpec "Big-endian dense chunked number parser" denseVarWordBe 34976
-  varNumSpec "Little-endian chunked number parser" varWordLe 526337
+  varNumSpec "Big-endian chunked number parser"
+    varWordBe Builder.varWordBe 18464
+  varNumSpec "Big-endian dense chunked number parser"
+    denseVarWordBe Builder.denseVarWordBe 34976
+  varNumSpec "Little-endian chunked number parser"
+    varWordLe Builder.varWordLe 526337
 
 
-varNumSpec :: String -> Parser Word32 -> Word32 -> Spec
-varNumSpec s p expected = describe s $ do
-  it "should parse a single byte" $ forAll (choose (0, 127)) $ \word8 ->
-    parseOnly p (BS.singleton word8) `shouldBe` Right (fromIntegral word8)
+varNumSpec :: String -> Parser Word32 -> (Word32 -> Builder) -> Word32 -> Spec
+varNumSpec s p b expected = describe s $ do
   it "should parse a known bytestring" $ do
     parseOnly p (BS.pack [0b10000001, 0b10010000, 0b00100000])
       `shouldBe` Right expected
+  it "should parse any valid single byte" $ forAll (choose (0, 127)) $ \word8 ->
+    parseOnly p (BS.singleton word8) `shouldBe` Right (fromIntegral word8)
+  it "should roundtrip any valid bytestring" $ property $ \word32 ->
+    let
+      encoded = LBS.toStrict $ Builder.toLazyByteString $ b word32
+    in
+      parseOnly p encoded `shouldBe` Right word32
